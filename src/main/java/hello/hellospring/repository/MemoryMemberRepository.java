8917fa7b9be7777efe17f04c2520e2672f04990d
YOOBINNOH <kuninyb@naver.com>
2022-02-15 11:02:32 +0900

    13. 컴포넌트 스캔 (@Component가 있으면 spring bean 에 등록이 된다. 그 예시는 @Repository, @Controller, @Service 가 있다.)

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
@@ -19,6 +21,7 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public Optional<Member> findById(Long id) {
+
         return Optional.ofNullable(store.get(id));
     }
 
